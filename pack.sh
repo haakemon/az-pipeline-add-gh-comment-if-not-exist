423b12678bec39a156508c2e59696da4c5983e1e
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-10-16 09:15:17 +0200

    feat: got and dont delete comment, just check if it already exists

--- pack.sh
+++ pack.sh
@@ -1,2 +1,5 @@
 rm ./build/*
+cd ./src
+./node_modules/.bin/tsc
+cd ..
 ./src/node_modules/.bin/tfx extension create --output-path ./build --manifest-globs vss-extension.json
--- src/main.ts
+++ src/main.ts
@@ -1,93 +1,126 @@
-// Used this as a starting point:
-// https://github.com/microsoft/azure-pipelines-tasks/blob/88ec5139c28625ea228d3a40f662454cd2f5dd44/Tasks/GitHubCommentV0/main.ts#L1
-
+// @ts-ignore
 import * as tl from 'azure-pipelines-task-lib/task';
-import { sendRequest, WebRequest, WebResponse } from './httpclient';
 import * as path from 'path';
+import got from 'got';
 
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
 function getGithubEndPointToken(githubEndpoint: string): string {
-    const githubEndpointObject = tl.getEndpointAuthorization(githubEndpoint, false);
-    let githubEndpointToken: string | undefined = undefined;
+  const githubEndpointObject = tl.getEndpointAuthorization(githubEndpoint, false);
+  let githubEndpointToken: string | undefined = undefined;
 
-    if (!!githubEndpointObject) {
-        tl.debug('Endpoint scheme: ' + githubEndpointObject.scheme);
+  if (!!githubEndpointObject) {
+    tl.debug('Endpoint scheme: ' + githubEndpointObject.scheme);
 
-        if (githubEndpointObject.scheme === 'PersonalAccessToken') {
-            githubEndpointToken = githubEndpointObject.parameters.accessToken;
-        } else if (githubEndpointObject.scheme === 'OAuth') {
-            githubEndpointToken = githubEndpointObject.parameters.AccessToken;
-        } else if (githubEndpointObject.scheme === 'Token') {
-            githubEndpointToken = githubEndpointObject.parameters.AccessToken;
-        } else if (githubEndpointObject.scheme) {
-            console.log(githubEndpointObject.scheme);
-            throw new Error('InvalidEndpointAuthScheme');
-        }
+    if (githubEndpointObject.scheme === 'PersonalAccessToken') {
+      githubEndpointToken = githubEndpointObject.parameters.accessToken;
+    } else if (githubEndpointObject.scheme === 'OAuth') {
+      githubEndpointToken = githubEndpointObject.parameters.AccessToken;
+    } else if (githubEndpointObject.scheme === 'Token') {
+      githubEndpointToken = githubEndpointObject.parameters.AccessToken;
+    } else if (githubEndpointObject.scheme) {
+      console.log(githubEndpointObject.scheme);
+      throw new Error('InvalidEndpointAuthScheme');
     }
+  }
 
-    if (!githubEndpointToken) {
-        console.log(githubEndpoint)
-        throw new Error('InvalidGitHubEndpoint');
-    }
+  if (!githubEndpointToken) {
+    console.log(githubEndpoint);
+    throw new Error('InvalidGitHubEndpoint');
+  }
 
-    return githubEndpointToken;
+  return githubEndpointToken;
 }
 
-function findComments(repositoryName: string, id: string, comment: string, token: string) {
-    const url = `https://api.github.com/repos/${repositoryName}/issues/${id}/comments`;
-    const headers = {
-        'Authorization': `token ${token}`,
-        'Content-Type': 'application/json'
-    };
+const getComments = async ({
+  repositoryName,
+  id,
+  token,
+}: {
+  repositoryName: string;
+  id: string;
+  token: string;
+}): Promise<Record<string, any>> => {
+  const url = `https://api.github.com/repos/${repositoryName}/issues/${id}/comments`;
+  const options = {
+    headers: {
+      Authorization: `token ${token}`,
+      'Content-Type': 'application/json',
+    },
+  };
 
-    const request = new WebRequest();
-    request.uri = url;
-    request.headers = headers;
-    request.method = 'GET';
+  try {
+    return await got(url, options).json();
+  } catch (error) {
+    throw new Error(`Fetching comments failed ${error}`);
+  }
+};
 
-    return sendRequest(request).then((response: WebResponse) => {
-        if (response.statusCode !== 201) {
-            tl.debug(JSON.stringify(response));
-            throw new Error('WriteFailed');
-        } else {
-            console.log(JSON.stringify(response));
-        }
-    });
-}
+const writeComment = async ({
+  repositoryName,
+  id,
+  token,
+  comment,
+}: {
+  repositoryName: string;
+  id: string;
+  token: string;
+  comment: string;
+}) => {
+  const url = `https://api.github.com/repos/${repositoryName}/issues/${id}/comments`;
+  const options = {
+    body: comment,
+    headers: {
+      Authorization: `token ${token}`,
+      'Content-Type': 'application/json',
+    },
+  };
 
-function run(): Promise<void> {
-    const endpointId = tl.getInput('gitHubConnection', true);
-    if (!endpointId) {
-        throw new Error("Could not determine endpointId");
-    }
-    const token = getGithubEndPointToken(endpointId);
+  try {
+    await got.post(url, options);
+  } catch (error) {
+    throw new Error(`Writing comment failed ${error}`);
+  }
+};
 
-    const repositoryName = tl.getInput('repositoryName', true);
-    if (!repositoryName) {
-        throw new Error("Could not determine repositoryName");
-    }
+const run = async (): Promise<void> => {
+  const endpointId = tl.getInput('gitHubConnection', true);
+  if (!endpointId) {
+    throw new Error('Could not determine endpointId');
+  }
+  const token = getGithubEndPointToken(endpointId);
 
-    if (!tl.getVariable('Build.SourceBranch') || tl.getVariable('Build.SourceBranch')!.startsWith('refs/pull/')) {
-        throw new Error("Could not determine Build.SourceBranch");
-    }
-    const id = tl.getVariable('Build.SourceBranch')!.split('/')[2];
+  const repositoryName = tl.getVariable('Build.Repository.Name');
+  if (!repositoryName) {
+    throw new Error('Could not determine Build.Repository.Name');
+  }
 
+  if (!tl.getVariable('Build.SourceBranch') && !tl.getVariable('Build.SourceBranch')!.startsWith('refs/pull/')) {
+    throw new Error('Could not determine Build.SourceBranch');
+  }
+  const id = tl.getVariable('Build.SourceBranch')!.split('/')[2]!;
 
-    const includesString = tl.getInput('includesString');
-    if (!includesString) {
-        throw new Error("Could not determine includesString");
-    }
+  const commentToPost = tl.getInput('comment');
+  if (!commentToPost) {
+    throw new Error('Could not determine comment');
+  }
 
-    if (!id) {
-        console.log('NoOp');
-        return Promise.resolve();
-    }
+  const comments = await getComments({repositoryName, id, token});
+  const hasComment = comments.some((githubCommentEntry: any) => {
+    const hasText = githubCommentEntry.body.includes(commentToPost);
 
+    return hasText;
+  });
 
-    return findComments(repositoryName, id, token, includesString);
-}
+  if (hasComment) {
+    console.log('Comment already exists, skipping add comment');
+  } else {
+    await writeComment({repositoryName, id, token, comment: commentToPost});
+  }
+
+  return Promise.resolve();
+};
 
 run()
-    .then(() => tl.setResult(tl.TaskResult.Succeeded, ''))
-    .catch((error: Error) => tl.setResult(tl.TaskResult.Failed, error.message));
+  .then(() => tl.setResult(tl.TaskResult.Succeeded, ''))
+  .catch((error: Error) => tl.setResult(tl.TaskResult.Failed, error.message));
